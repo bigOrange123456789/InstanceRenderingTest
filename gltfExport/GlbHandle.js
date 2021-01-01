225f89715b8416640d55404dbd2d90404bcb6d75
bigOrange123456789 <2814255951@qq.com>
2021-01-01 14:33:13 +0800

    拆分算法取得了成功，超爽的

--- gltfExport/GlbHandle.js
+++ gltfExport/GlbHandle.js
@@ -1,8 +1,40 @@
 function GlbHandle(){
     this.counter=0;//计数器，记录处理文件的个数，初始值为0
+    this.dataArray=[];
+    this.arrayI=0;
+    this.arrayJ=0;
 }
 GlbHandle.prototype={
+    arrayDownLoad:function(){
+        var myArray=this.dataArray;
+        var i=this.arrayI,j=this.arrayJ,index=this.counter;
+        if(i===myArray.length)return;//已经全部输出
+        console.log(myArray.length,":",i,j,index);
+        var mesh,name;
+        if(myArray[i].children.length!==0) {//为Group类型，有孩子
+            mesh=myArray[i].children[j].clone();
+            //指定下一个mesh
+            if(j===myArray[i].children.length-1){
+                i++;
+                j=0;
+            }else{
+                j++;
+            }
+        } else {//纯mesh类型
+            mesh=myArray[i].clone();
+            //指定下一个mesh
+            i++;//j=0;
+        }
 
+        name='new'+index+'.gltf';
+        index++;
+
+        this.meshDownLoad(mesh,name);
+
+        this.arrayI=i;
+        this.arrayJ=j;
+        this.counter=index;
+    },
     meshDownLoad:function (mesh,name){
         var scene=new THREE.Scene();
         scene.add(mesh);
@@ -22,6 +54,12 @@ GlbHandle.prototype={
         link.href = URL.createObjectURL(new Blob([JSON.stringify(result)], { type: 'text/plain' }));
         link.download = name;
         link.click();
-        document.body.removeChild(link);
+
+        var scope=this;
+        console.log(name);
+        setTimeout((function (){
+            scope.arrayDownLoad();
+        }),500);
+        //document.body.removeChild(link);
     },
 }
