391d671b0e903ebd153b2a7c45387fb997ae4eae
bigOrange123456789 <2814255951@qq.com>
2021-04-25 01:22:49 +0800

    save

--- DATA/name.js
+++
@@ -1,6 +0,0 @@
-    var fs = require('fs');
-    var result=[]
-    fs.readdirSync(process.argv[2]).forEach(function (s) {
-        result.push(s);
-    });
-    fs.writeFile('name.json' , JSON.stringify(result , null, "\t") , function(){});
