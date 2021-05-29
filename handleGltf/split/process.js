b7a93a5ae95b2dcbdae4f19a271d71c13143c7c5
bigOrange123456789 <2814255951@qq.com>
2021-05-30 02:56:44 +0800

    删除前判断是否存在

--- handleGltf/split/process.js
+++ handleGltf/split/process.js
@@ -14,10 +14,17 @@ function getInf(){
 }
 function deleteFiles() {
     var path="../../../_DATA_/model_sim/"
+    var haveList=[]
+    fs.readdirSync(path).forEach(function (s) {
+        haveList.push(s)
+    });
+    console.log(haveList)
     for(var i=0;i<names.length;i++){
         console.log(names[i],inf[names[i]].url)
         if(names[i]!==inf[names[i]].url){
-            fs.unlinkSync(path  + names[i]);
+            if(haveList[names[i]])
+                fs.unlinkSync(path  + names[i]);
         }
     }
+    console.log("finish!")
 }
