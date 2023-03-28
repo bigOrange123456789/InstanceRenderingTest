990f83bbf4f675192fcd10d8d24880e0cf89409e
bigOrange123456789 <bigOrange123456789>
2023-03-29 00:18:30 +0800

    Entropy of color representation

---
+++ npm/npm_demo8(Entropy of color representation)/src/Building_new.js
@@ -0,0 +1,137 @@
+import * as THREE from "three";
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader"
+import {OBJExporter} from "three/examples/jsm/exporters/OBJExporter"
+import { saveAs } from 'file-saver';
+import config from '../config/config.json'
+
+export class Building{
+    constructor(scene){
+        this.config=config.src.Building_new
+        this.parentGroup = new THREE.Group()
+        scene.add(this.parentGroup)
+        this.load0(this.config.path)
+        this.createSphere(this.config.createSphere)
+    }
+    createSphere(c){
+        let self=this
+        let entropyMax=0.001
+        for(let name in self.config.entropy){
+            if(self.config.entropy[name]>entropyMax)entropyMax=self.config.entropy[name]
+        }
+        let test=[]
+        console.log(self.config["kernelPosition"])
+        for(let x=c.x[0];x<=c.x[1];x=x+c.x[2])
+            for(let y=c.y[0];y<=c.y[1];y=y+c.y[2])
+                for(let z=c.z[0];z<=c.z[1];z=z+c.z[2]){
+                    test.push([x,y,z])
+                    let geometry,material
+                    let name=(x+","+y+","+z)
+                    if(name==self.config["kernelPosition"]){
+                        geometry = new THREE.SphereGeometry( c.r, 32, 16 );
+                        material = new THREE.MeshBasicMaterial( {color:0xff0000} );
+                        console.log(material.color)
+                        // alert(1)
+                    }else{
+                        geometry = new THREE.SphereGeometry( c.r, 32, 16 );
+                        material = new THREE.MeshBasicMaterial( {color:0x00ffff} );
+                        material.color.r=material.color.g=material.color.b=
+                            self.config.entropy[name]/entropyMax
+                        
+                    }
+                    
+                    const sphere = new THREE.Mesh( geometry, material );
+                    sphere.position.set(x,y,z)
+                    self.parentGroup.add( sphere )
+                    if( name==self.config["kernelPosition"] )window.sphere=sphere
+
+                }
+        console.log("test",test)
+    }
+    load0(path){
+        var self=this
+        const loader = new GLTFLoader();
+        loader.load(path, function (gltf) {
+            let arr=gltf.scene.children[0].children[0].children
+            const arr2=[]
+            let matrices_all=""//[]
+            for(let i=0;i<arr.length;i++){
+                var mesh=arr[i]
+                var name=mesh.name
+                var id=parseInt(name.split("_")[1])
+                // if(id>=2)mesh.visible=false
+                mesh.id0=id
+                arr2.push(mesh)
+                // console.log(mesh.material)
+                // var color=mesh.material.color
+                // color.r=0
+                // color.g=id/15
+                // color.b=0
+
+                matrices_all=matrices_all+"[], "//.push([])
+
+                // var material2=new THREE.MeshBasicMaterial({color:id})
+                // material2.color.convertSRGBToLinear();
+                // var mesh2=new THREE.Mesh(mesh.geometry,material2)
+                // self.parentGroup.add(mesh2)
+                // mesh2.matrixWorld=mesh.matrixWorld
+                // mesh.visible=false
+            }
+            console.log(arr)
+            self.parentGroup.add(gltf.scene)
+            console.log(matrices_all)
+
+            arr=arr2
+            const loading = function(index){
+                console.log(index,arr[index].id0+".obj")
+                self.saveMesh(arr[index],arr[index].id0+".obj")
+                if(index+1>=arr.length) {
+                    console.log("finish!")
+                }else{
+                    setTimeout(()=>{
+                        loading(index+1)
+                    },100)
+                }
+            }
+            window.download=()=>{
+                loading(0)
+            }
+            
+        }, undefined, function (error) {
+            console.error(error);
+        });
+
+    }
+    saveMesh(mesh,name){
+        const scene=new THREE.Scene()
+        mesh.geometry.attributes.normal=null
+        mesh.geometry.attributes={
+            position:mesh.geometry.attributes.position
+        }
+        scene.add(mesh)
+        const objData = new OBJExporter().parse(scene, { includeNormals: false });
+
+        // 将数据保存为OBJ文件
+        const blob = new Blob([objData], { type: 'textain' });
+        saveAs(blob, name);
+    }
+    saveJson(myObj,name){
+        // 将JSON对象转换为JSON字符串
+        var jsonString = JSON.stringify(myObj);
+
+        // 创建一个Blob对象，将JSON字符串作为数据
+        var blob = new Blob([jsonString], { type: "application/json" });
+
+        // 创建一个链接，指向这个Blob对象
+        var url = URL.createObjectURL(blob);
+
+        // 创建一个a标签，设置下载链接和文件名
+        var a = document.createElement("a");
+        a.href = url;
+        a.download = name;
+
+        // 模拟点击a标签，触发文件下载
+        a.click();
+    }
+
+
+}
---
+++ npm/npm_demo8(Entropy of color representation)/src/sampling/main_sampling.js
@@ -0,0 +1,133 @@
+import * as THREE from "three";
+import Stats from "three/examples/jsm/libs/stats.module.js";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
+import { PlayerControl } from '../lib/playerControl/PlayerControl.js'
+//RGBMLoader
+import { Building } from './Building_new_sampling'
+import {Panel } from './Panel.js'
+import config from '../config/config.json'
+export class Loader{
+    constructor(body){
+        this.config=config.src.main
+        this.body = body
+        this.canvas = document.getElementById('myCanvas')
+        window.addEventListener('resize', this.resize.bind(this), false)
+        this.initScene()
+        // this.addMyUI()
+        new Panel()
+    }
+    async initScene(){
+        this.renderer = new THREE.WebGLRenderer({
+            // alpha:true,
+            // antialias: true,
+            // canvas:this.canvas,
+            // preserveDrawingBuffer:true
+            antialias: false,//关闭抗锯齿
+            preserveDrawingBuffer: true//将每一帧缓存到renderer.domElement中
+        })
+        ////////////////////////////////////////////////////////////////////////
+        // this.renderer.physicallyCorrectLights = true //正确的物理灯光照射
+        // this.renderer.outputEncoding = THREE.sRGBEncoding //采用sRGBEncoding 
+        // this.renderer.toneMapping = THREE.ACESFilmicToneMapping //aces标准
+        // this.renderer.toneMappingExposure = 1//1.25 //调映射曝光度
+        // this.renderer.shadowMap.enabled = true //阴影
+        // this.renderer.shadowMap.type = THREE.PCFSoftShadowMap //阴影类型（处理运用Shadow Map产生的阴影锯齿）
+        ////////////////////////////////////////////////////////////////////////
+
+        this.renderer.setSize(800,800)//(this.body.clientWidth,this.body.clientHeight)
+        this.renderer.setPixelRatio(window.devicePixelRatio)
+        window.renderer=this.renderer
+        this.body.appendChild(this.renderer.domElement)
+        
+        this.renderer.outputEncoding = THREE.sRGBEncoding;
+        this.renderer.setClearColor(0xffffff);//(0xcccccc);
+
+        this.renderer.autoClear = true;
+
+
+
+        this.stats = new Stats();
+        this.stats.domElement.style.position = 'absolute'
+        this.stats.domElement.style.left = '0px'
+        this.stats.domElement.style.top = '0px'
+        var statsContainer = document.createElement('div')
+        statsContainer.id = 'stats-container'
+        statsContainer.appendChild(this.stats.domElement)
+        this.body.appendChild(statsContainer)
+
+        this.scene = new THREE.Scene()
+
+        this.camera = new THREE.PerspectiveCamera(90,this.body.clientWidth/this.body.clientHeight,0.01,5000)
+        // this.camera.up.set(0,0,1)
+        // this.camera.position.set(-43.486343682038736,  2.127206120237504,  -8.698678933445201)
+        this.camera.position.set(1780.71101881,  1600.5785283,  14.783995491)
+        this.camera.position.set(1874.4326513789465,  1684.8195034736839,  15.56210051684223)
+        window.camera=this.camera
+        this.camera.lookAt(0,0,0)
+        console.log(this.config)
+        this.camera.position.set(
+            this.config.camera.position.x,
+            this.config.camera.position.y,
+            this.config.camera.position.z
+        )
+        this.camera.rotation.set(
+            this.config.camera.rotation.x,
+            this.config.camera.rotation.y,
+            this.config.camera.rotation.z
+        )
+
+        this.camera.name = "camera";
+        // this.camera.position.set( -0.6821012446503002,  11.0913040259869,  -0.2459109391034793)
+        // this.camera.rotation.set(-1.5929642031588853,  -0.061406332932874515, -1.9174927752336077)
+        window.camera=this.camera
+        
+        this.scene.add(this.camera)
+        
+        // this.orbitControl = new OrbitControls(this.camera,this.renderer.domElement)
+        // this.orbitControl.target.set(1780.71101881,  1600.5785283,  14.783995491)
+
+        this.playerControl=new PlayerControl(this.camera)
+        this.playerControl.mode.set("viewpoint")
+        console.log(this.playerControl)
+        this.playerControl.target.set(1780.71101881,  1600.5785283,  14.783995491)
+
+        
+        this.animate = this.animate.bind(this)
+        requestAnimationFrame(this.animate)
+
+        // new AvatarManager(this.scene,this.camera)
+        this.scene.add(new THREE.DirectionalLight(0xFFFFFF, 3.5));//new LightProducer(this.scene)
+
+        // const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        // const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+        // const cube = new THREE.Mesh( geometry, material );
+        // cube.position.set(1780.71101881,  1600.5785283,  14.783995491)
+        // this.scene.add( cube );
+
+        new Building(this.scene,this.camera)
+    }
+    animate(){
+        // let startTime = Date.now();
+        // if (this.lastTime) {
+        //   console.log("每帧时间",Date.now()-this.lastTime)
+        // }
+        // this.lastTime = Date.now();
+        this.stats.update()
+        // this.effectComposer.render()
+        //如果要启用后处理，就需要用后处理通道覆盖render通道  
+        this.renderer.render(this.scene,this.camera)
+        // let endTime = Date.now()
+        // console.log("main时间", endTime - startTime)
+        requestAnimationFrame(this.animate)
+    }
+    resize(){
+        this.canvas.width = 800//window.innerWidth;//this.body.clientWidth
+        this.canvas.height = 800//window.innerHeight;//this.body.clientHeight
+        this.camera.aspect = 1//this.canvas.width/this.canvas.height;//clientWidth / clientHeight
+        this.camera.updateProjectionMatrix()
+        this.renderer.setSize(800,800)//(this.canvas.width, this.canvas.height)
+    }
+}
+document.addEventListener('DOMContentLoaded', () => {
+    new Loader(document.body)
+})
